a8c8a65efafd6889f0f1007231ed5c75f392e2fe
Xun Qiao <xun.qiao@microfocus.com>
2019-04-28 21:31:02 +0800

    temp save

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import {router} from './test/router';
 import './assets/css/bootstrap.min.css';
+import './plugins/vuetify'
 
 // load js
 if (require) {
